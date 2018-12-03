500589bf6f70356499d70dff966cf54ea0db0636
James Hotblack <james.c.hotblack@gmail.com>
2018-12-03 16:47:11 +0000

    Refactored balance mthod to attr_reader = :balance and added initialize method for default value for balance = 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,8 @@
 require 'oystercard.rb'
 
 describe Oystercard do
-
+  it "Does oystercard respond to balance method" do
+    subject = Oystercard.new
+    expect(subject.balance).to eq 0
+  end
 end
