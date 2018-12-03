d41827ab70063628f0d3d0e5562c0dd3b64a1e2c
James Hotblack <james.c.hotblack@gmail.com>
2018-12-03 17:07:09 +0000

    Made a passing test for toppingup balance by 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,9 @@ describe Oystercard do
     subject = Oystercard.new
     expect(subject.balance).to eq 0
   end
+
+  it "tops up card" do
+    subject = Oystercard.new
+    expect { subject.top_up(5) }.to change { subject.balance }.by(5)
+  end
 end
