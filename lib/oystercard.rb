4e8ac1b5131568daf4d4ba22e2b5bdb346138da6
James Hotblack <james.c.hotblack@gmail.com>
2018-12-03 17:40:33 +0000

    Step 6 Changed functionality of top up method to raise error for max balance. Max balance saved as constant.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
+
 attr_reader :balance
+MAX_BALANCE = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(value)
+    raise "Max Balance reached" if @balance + value >= MAX_BALANCE
     @balance += value
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,8 +6,17 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
-  it "tops up card" do
-    subject = Oystercard.new
-    expect { subject.top_up(5) }.to change { subject.balance }.by(5)
+  context "Top up" do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it "tops up card" do
+      subject = Oystercard.new
+      expect { subject.top_up(5) }.to change { subject.balance }.by(5)
+    end
+
+    it "raises an error if maximum balance is hit" do
+      subject = Oystercard.new
+      expect { subject.top_up(100) }.to raise_error "Max Balance reached"
+    end
   end
 end
