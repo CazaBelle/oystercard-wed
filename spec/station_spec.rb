2d529e0bb6bfd26b373b61bbd31ab476b543d6c6
Carol Gardiner <carolan.gardiner@gmail.com>
2018-12-05 16:54:03 +0000

    step 13: added a name and zone double

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,16 +1,17 @@
 require 'station.rb'
 
 RSpec.describe Station do 
-# let(:name) { double :name }
+let(:name) { double :name }
+let(:zone) { double :zone }
 
 
   it "will expose a name at initialization" do
-    station = Station.new("liverpool", "zone 2")
+    station = Station.new(:name, :zone)
     expect(station).to respond_to(:name)
   end
   
   it "will expose a zone at initialization" do
-    station = Station.new("liverpool", "zone 2")
+    station = Station.new(:name, :zone)
     expect(station).to respond_to(:zone)
   end
 
